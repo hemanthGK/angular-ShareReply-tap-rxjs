08983c5e90af163ec67e6f53e7f4e138ec3f8e52
krish5596 <hemanthkumar.k@contus.in>
2020-09-21 19:49:04 +0530

    init

---
+++ src/app/item-list/item-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ItemService } from '../item.service'
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
+})
+export class ItemListComponent implements OnInit {
+
+
+  items = []
+
+  constructor(private itemService: ItemService) { }
+
+  ngOnInit(): void {
+    this.itemService.getItems()
+      .subscribe((items: any) => {
+        this.items = items;
+      })
+  }
+
+
+}
